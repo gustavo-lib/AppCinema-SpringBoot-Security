f6a2dbd065754ba2c3af4962fd55d7bc051c51ed
gortiz <ggortiz09@gmail.com>
2021-04-15 11:00:54 -0300

    fix controller path /login

--- src/main/java/com/gortiz/AppCinema/Controller/LoginController.java
+++ src/main/java/com/gortiz/AppCinema/Controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
 	
     
-	@GetMapping
+	@GetMapping("/login")
 	public String login() {
         return "login";
     }
