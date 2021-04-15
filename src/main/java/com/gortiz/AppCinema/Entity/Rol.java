85164ce47219b0d34f95ab856a1b48b54a58c33e
gortiz <ggortiz09@gmail.com>
2021-04-15 16:59:23 -0300

    update entity rol

--- src/main/java/com/gortiz/AppCinema/Entity/Rol.java
+++ src/main/java/com/gortiz/AppCinema/Entity/Rol.java
@@ -5,43 +5,47 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import com.gortiz.AppCinema.Enum.RolNombre;
 
 /**
  *
  * @author Gustavo
  */
+import com.sun.istack.NotNull;
+//import com.gortiz.AppCinema.Security.;
+
+import javax.persistence.*;
+
 @Entity
 public class Rol {
-    
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-    Long id;
-    private String nombre;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true)
+    private RolNombre rolNombre;
 
-    /**
-     * Constructor por Defecto
-     */
     public Rol() {
     }
-   
-    /**
-     * Este constructor incluye todos los atributos de la clase
-     * 
-     * @param nombre
-     */
-    public Rol(String nombre) {
-        this.nombre = nombre;
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
     }
 
-    // A continuación se listan todos los métodos 
-    // de seteo de cada atributo de la clase
-    
-    public String getNombre() {
-        return nombre;
+    public int getId() {
+        return id;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setId(int id) {
+        this.id = id;
     }
-    
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
 }
