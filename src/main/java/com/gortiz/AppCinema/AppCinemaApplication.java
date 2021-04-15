c79371dd2ac8e275dff127f995a2e6e2b9b937fa
gortiz <ggortiz09@gmail.com>
2021-04-15 10:24:37 -0300

    first commit App Cinema

---
+++ src/main/java/com/gortiz/AppCinema/AppCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.gortiz.AppCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCinemaApplication.class, args);
+	}
+
+}
