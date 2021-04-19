f801bb98d10bc0b1214f2d3ebdcd9dd468aeaa5f
gortiz <ggortiz09@gmail.com>
2021-04-19 12:04:58 -0300

    controller login logout

--- src/main/java/com/gortiz/AppCinema/Controller/LoginController.java
+++ src/main/java/com/gortiz/AppCinema/Controller/LoginController.java
@@ -1,9 +1,15 @@
 package com.gortiz.AppCinema.Controller;
 
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
+@RequestMapping("/")
 public class LoginController {
 	
     
@@ -12,9 +18,29 @@ public class LoginController {
         return "login";
     }
 	
-	@GetMapping("/welcome")
-	public String index() {
-        return "welcome";
+	@GetMapping("/success")
+	public String index2() {
+        return "default";
     }
-
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@GetMapping("/admin")
+	public String index_admin() {
+        return "admin";
+    }
+	
+	@PreAuthorize("hasRole('ROLE_USER')")
+	@GetMapping("/user")
+	public String index_user() {
+        return "user";
+    }
+    @GetMapping("/forbidden")
+    public String forbidden(){
+        return "forbidden";
+    }
+    
+    /*@PreAuthorize("hasRole('ADMIN')")
+	@GetMapping("/formpelicula")
+	String pelicula() {
+		return "formpeli";
+	}*/
 }
