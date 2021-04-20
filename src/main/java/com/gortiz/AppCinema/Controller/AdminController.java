7fa5e49b8dae9f189701e6668aff514d852bfffd
gortiz <ggortiz09@gmail.com>
2021-04-20 18:58:31 -0300

    update html, css

--- src/main/java/com/gortiz/AppCinema/Controller/AdminController.java
+++ src/main/java/com/gortiz/AppCinema/Controller/AdminController.java
@@ -1,24 +1,52 @@
 package com.gortiz.AppCinema.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gortiz.AppCinema.Entity.Pelicula;
+import com.gortiz.AppCinema.Serivices.IPelicula;
 
 @Controller
 public class AdminController {
 	
+	
+	@Autowired
+	IPelicula servicePelicula;
+	
 	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/listapelicula")
 	String listapelicula() {
+		
 		return "lista_admin";
 	}
 	
-	
 	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/formpelicula")
-	String pelicula() {
+	String pelicula(Model model) {
+		Pelicula pelicula = new Pelicula();
+		pelicula.setCalificacion(null);
+		pelicula.setPersonajes(null);
+		pelicula.setPaisDeOrigen(null);
+		pelicula.setFunciones(null);
+		model.addAttribute("pelicula", pelicula);
 		return "formpeli";
 	}
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@PostMapping("/guardar")
+	String guardar_pelicula(@ModelAttribute("pelicula") Pelicula pelicula) {
+		servicePelicula.save(pelicula);
+		return "redirect:/success";
+	}
+
+	
+	
+	
 	
 }
--- src/main/java/com/gortiz/AppCinema/Controller/LoginController.java
+++ src/main/java/com/gortiz/AppCinema/Controller/LoginController.java
@@ -2,15 +2,22 @@ package com.gortiz.AppCinema.Controller;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gortiz.AppCinema.Serivices.IPelicula;
 
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
+	
+	@Autowired
+	IPelicula servicePelicula;
 	
     
 	@GetMapping({"/login", "/"})
@@ -19,8 +26,11 @@ public class LoginController {
     }
 	
 	@GetMapping("/success")
-	public String index2() {
-        return "default";
+	public ModelAndView index2() {
+		ModelAndView model=new ModelAndView("default");
+		model.addObject("list_pelicula", servicePelicula.getAllPelicula());
+		return model;
+        //return "default";
     }
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@GetMapping("/admin")
